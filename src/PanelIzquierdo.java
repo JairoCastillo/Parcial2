873c09395acc308f91ef31537863be5819002932
Unknown <brayangarciaxd@gmail.com>
2017-10-17 08:20:09 -0500

    panelI

--- src/PanelIzquierdo.java
+++ src/PanelIzquierdo.java
@@ -1,4 +1,16 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class PanelIzquierdo {
-
+	private String nombre,apellido,direccion,tipo;
+	private int cedula,numero,edad,peso,altura,celular,puesto;
+	
+	private JLabel Lnombre,Lapellido,Ltipo,Lnumero,Ledad,Lpeso,Laltura,Lcelular,Ldireccion,Lpuesto;
+	private JButton Bagregar;
+	private JTextField Tnombre,Tapellido,Ttipo,Tnumero,Tedad,Tpeso,Taltura,Tcelular,Tdireccion,Tpuesto;
+	
+	public PanelIzquierdo() {
+		
+	}
 }
